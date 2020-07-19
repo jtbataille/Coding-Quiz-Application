473fad90a4c3fcf65ca89a4abedaac3059058fd7
Jarrod Bataille <jtbataille@me.com>
2020-07-19 19:37:39 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$("button").on("click", function (e) {
+    var timeLeft = 75;
+    var elem = document.getElementById("countdown");
+    var timerId = setInterval(countdown, 1000);
+
+    function countdown() {
+        if (timeLeft == -1) {
+            clearTimeout(timerId);
+            doSomething();
+        } else {
+            elem.innerHTML = "Time: " + timeLeft;
+            timeLeft--;
+        }
+    }
+});
